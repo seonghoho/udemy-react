1c8359ab3434ba8634836c4b269c36a94bee4fc8
최성호 <chltjdgh3@naver.com>
2023-12-11 01:19:27 +0900

    [231210] 자바스크립트 문법 응용 학습

---
+++ 02_Javascript_hard/index.js
@@ -0,0 +1,11 @@
+let response = fetch('https://jsonplaceholder.typicode.com/posts').then((res)=>
+    console.log(res)
+);
+
+async function getData (){
+    let rawResponse = await fetch("https://jsonplaceholder.typicode.com/posts");
+    let jsonResponse = await rawResponse.json();
+    console.log(jsonResponse);
+}
+
+getData();
