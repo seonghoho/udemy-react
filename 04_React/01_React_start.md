db09b00b4002d681186842974a079fd6cbe18378
최성호 <chltjdgh3@naver.com>
2023-12-13 23:49:33 +0900

    [231213] React 기초 학습

---
+++ 04_React/01_React_start.md
@@ -0,0 +1,76 @@
+# React app 설치하기
+
+npx와 node를 설치 완료했다면, 아래 코드를 적어 app을 설치한다.
+
+```
+npx create-react-app reactexam1
+```
+
+참고로 npx는 패키지를 로컬에 설치하지 않고, 한 번만 사용할 때 사용하는 명령어다.
+
+
+## ES 모듈 시스템
+
+```
+export default App;
+```
+
+위의 코드를 사용해 function App을 내보낼 수 있고,
+
+```
+import App from './App';
+```
+
+이 코드로 불러와 사용할 수 있다.
+
+
+## 컴포넌트
+
+React에서는 Vue와 마찬가지로, 기능별로 컴포넌트를 만들어 사용한다.
+
+
+## React Fragment
+
+```
+import React from 'react';
+
+<React.Fragment>
+```
+
+fragment 태그 안에 넣은 내용은 최상위 태그에 적용되지 않는다.
+
+<></> 빈 태그로 작성해도 문제되지 않는다.
+
+## css 적용하는 방법
+
+### 별도의 css 파일 적용하는 방법
+
+```js
+<div className="App">
+    <MyHeader/>
+    <h2>안녕 리액트
+    <p>{name} 입니다.</p>
+    </h2>
+    <b id='bold_text'>React.js</b>
+    <MyFooter/>
+</div>
+```
+
+### 인라인 스타일 적용하는 법
+
+```js
+<div style={style.App}>
+    <MyHeader/>
+    <h2 style={style.h2}>안녕 리액트
+    <p>{name} 입니다.</p>
+    </h2>
+    <b style={style.bold_text}>React.js</b>
+    <MyFooter/>
+</div>
+```
+
+#### jsx의 중괄호에는 숫자나 문자, 식을 포함할 수 있다.
+
+
+## 상태
+
---
+++ 04_React/reactexam1/src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+// import './App.css';
+
+import MyHeader from './Myheader';
+import MyFooter from './Myfooter';
+
+function App() {
+  let name = '최성호';
+
+  const style = {
+    App: {
+      backgroundColor: "black",
+    },
+    h2: {
+      color:"red",
+    },
+    bold_text: {
+      color:"green",
+    },
+  };
+
+  const func = () => {
+    return "func";
+  };
+
+  const number = 5;
+
+  return (
+    <React.Fragment>
+    {/* css 파일을 사용해서 적용하는 방법 */}
+    {/* <div className="App">
+      <MyHeader/>
+        <h2>안녕 리액트
+        <p>{name} 입니다.</p>
+        </h2>
+        <b id='bold_text'>React.js</b>
+      <MyFooter/>
+    </div> */}
+
+    {/* 인라인 스타일 적용하는 법 */}
+    <div style={style.App}>
+      <MyHeader/>
+        <h2 style={style.h2}>안녕 리액트 {func()}
+        <p>{name} 입니다.</p>
+        </h2>
+        <b style={style.bold_text}>React.js 
+        <br></br>
+        {number}는 : {number % 2 === 0 ? "짝수" : "홀수"}
+        </b>
+
+      <MyFooter/>
+    </div>
+  </React.Fragment>
+  );
+}
+
+export default App;
