e9b2136f5dbf37fa0f01bcd986c4a314a5c0f668
choiseongho <chltjdgh3@naver.com>
2023-12-17 23:05:51 +0900

    [231217] React diary project - List에 데이터 수정하기

--- 05_Simple_diary/simplediary/src/App.js
+++ 05_Simple_diary/simplediary/src/App.js
@@ -57,18 +57,25 @@ function App() {
   };
 
   // 게시글 삭제
-  const onDelete = (targetId) => {
+  const onRemove = (targetId) => {
     console.log(`${targetId}가 삭제되었습니다.`);
     const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
   };
 
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it)=>
+        it.id === targetId ? {...it, content:newContent } : it
+      )
+    );
+  };
 
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
       {/* <DiaryList diaryList={dummyList} /> */}
-      <DiaryList onDelete={onDelete} diaryList={data} />
+      <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
 }
