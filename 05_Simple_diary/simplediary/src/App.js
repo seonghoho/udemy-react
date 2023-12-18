7d8febdca13f4f2b63314d00c2a9bfb2b8d71d9a
choiseongho <chltjdgh3@naver.com>
2023-12-18 17:26:16 +0900

    [231218] React diary project - useReduce 적용

--- 05_Simple_diary/simplediary/src/App.js
+++ 05_Simple_diary/simplediary/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-import {useCallback, useEffect, useMemo, useRef, useState} from 'react';
+import {useCallback, useEffect, useMemo, useReducer, useRef } from 'react';
 // import OptimizeTest from './OptimizeTest';
 // import Lifecycle from './Lifecycle';
 
@@ -9,9 +9,48 @@ import {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 
 const App = () => {
 
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
 
-  const dateId = useRef(0);
+  const reducer = (state, action) => {
+    switch(action.type){
+      case 'INIT': {
+        return action.data
+      }
+
+      case 'CREATE': {
+        const created_date = new Date().getTime();
+        const newItem = {
+          ...action.data,
+          created_date
+        }
+        return [newItem, ...state];
+      }
+
+      case 'REMOVE': {
+        return state.filter((it)=> it.id !== action.targetId);
+      }
+
+      // action이 발생하면 targetId와 newContent가 전달된다.
+      // 기존 state에서 map함수를 사용해 targetId와 일치하는 요소를 찾은 다음, 
+      // 그 요소의 값은 content만 new content로 수정해주고, 나머지는 다시 돌려주고 
+      // 그 요소를 합쳐서 새로운 배열을 만들어서 새로운 state에 보내기 때문에 정상적으로 수행할 수 있다.
+      
+      case "EDIT": {
+        return state.map((it)=> 
+        it.id === action.targetId ? 
+        {...it, content:action.newContent} : it
+        );
+      }
+      default :
+      return state;
+    }
+  }
+
+
+  const [data, dispatch] = useReducer(reducer, [])
+
+
+  const dataId = useRef(0);
 
   const getData = async()=> {
     const res = await fetch(
@@ -24,11 +63,12 @@ const App = () => {
         content : it.body,
         emotion : Math.floor(Math.random() * 5)+1,
         created_date : new Date().getTime(),
-        id : dateId.current++
+        id : dataId.current++
       };
     });
 
-    setData(initData);
+    dispatch({type:"INIT", data:initData});
+    // setData(initData);
   };
 
   useEffect(()=>{
@@ -38,34 +78,47 @@ const App = () => {
   // 게시글 생성
   const onCreate = useCallback(
     (author, content, emotion) => {
-    const created_date = new Date().getTime();
-    const newItem = {
-      author,
-      content,
-      emotion,
-      created_date,
-      id : dateId.current
-    }
-    dateId.current += 1;
+    dispatch({
+      type:'CREATE', 
+      data: {author, content, emotion, id:dataId.current},
+    });
+    
+    // const created_date = new Date().getTime();
+    // const newItem = {
+    //   author,
+    //   content,
+    //   emotion,
+    //   created_date,
+    //   id : dataId.current
+    // }
+    dataId.current += 1;
+
     // newItem을 먼저 적으면 작성한 게시글이 위로 가게 만들 수 있다.
     // 인자로 데이터를 받아서 아이템을 추가한 데이터를 리턴하는 콜백함수를 setData에 전달할 것이다.
-    setData((data)=>[newItem, ...data])
+    
+    // setData((data)=>[newItem, ...data])
   }, 
   
-  []);
+  []
+  );
 
   // 게시글 삭제
   const onRemove = useCallback((targetId) => {
-    console.log(`${targetId}가 삭제되었습니다.`);
-    setData(data => data.filter((it) => it.id !== targetId));
+    dispatch({type: "REMOVE", targetId})
+
+    // console.log(`${targetId}가 삭제되었습니다.`);
+    // setData(data => data.filter((it) => it.id !== targetId));
   }, []);
 
   const onEdit = useCallback((targetId, newContent) => {
-    setData((data) =>
-      data.map((it)=>
-        it.id === targetId ? {...it, content:newContent } : it
-      )
-    );
+
+    dispatch({type: "EDIT", targetId, newContent})
+
+    // setData((data) =>
+    //   data.map((it)=>
+    //     it.id === targetId ? {...it, content:newContent } : it
+    //   )
+    // );
   }, []);
 
   const getDiaryAnalysis = useMemo(
