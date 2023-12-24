593f1e74c400661a49cfa2002655cca1875c0177
choiseongho <chltjdgh3@naver.com>
2023-12-24 10:06:20 +0900

    [231224] React diary project - Router 응용

--- 06_Emotion_diary/emotion_diary/src/pages/Edit.js
+++ 06_Emotion_diary/emotion_diary/src/pages/Edit.js
@@ -1,8 +1,35 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+
+    // useNavigate() 함수는 경로를 옮기는 함수이다. 로그인 안했으면 로그인 페이지로 보내는 경우 사용
+    const navigate = useNavigate();
+
+    // 
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const id = searchParams.get('id');
+    console.log("id : ", id);
+
+    const mode = searchParams.get("mode");
+    console.log("mode : ", mode);
+
+
     return (
         <div>
             <h1>Edit</h1>
             <p>이 곳은 일기 수정 페이지 입니다.</p>
+            <button onClick={() => setSearchParams({ who : "winterlood" })}>QS 바꾸기</button>
+            <button onClick={() => {
+                navigate("/home");
+            }}>
+                HOME으로 가기
+            </button>
+            <button onClick={()=>{
+                navigate(-1)
+            }}>
+                뒤로가기
+            </button>
         </div>
     );
 };
