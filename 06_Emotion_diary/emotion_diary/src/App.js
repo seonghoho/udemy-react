6fc08015e11eb9e1f24727f9658f29c9e0f5487a
choiseongho <chltjdgh3@naver.com>
2023-12-24 13:38:48 +0900

    [231224] React diary project - 기초 공사

--- 06_Emotion_diary/emotion_diary/src/App.js
+++ 06_Emotion_diary/emotion_diary/src/App.js
@@ -8,6 +8,10 @@ import New from './pages/New'
 import Edit from './pages/Edit'
 import Diary from './pages/Diary'
 
+// COMPONENTS
+import MyButton from './components/MyButton';
+import MyHeader from './components/MyHeader';
+
 function App() {
 
   // 이미지가 뜨지 않을 경우
@@ -17,7 +21,38 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h2>App.js</h2>
+
+      <MyHeader 
+        headText={'Header'} 
+        leftChild={
+          <MyButton  text={"왼쪽 버튼"} onClick={()=> alert("왼쪽 클릭")}/>
+        }      
+        rightChild={
+          <MyButton  text={"오른쪽 버튼"} onClick={()=> alert("오른쪽 클릭")}/>
+        }
+      />
+
+    <h2>App.js</h2>
+
+    <MyButton 
+      text={'버튼'} 
+      onClick={()=>alert("버튼 클릭")} 
+      type={"positive"}
+    />
+
+    <MyButton 
+      text={'버튼'} 
+      onClick={()=>alert("버튼 클릭")} 
+      type={"negative"}
+    />
+
+    <MyButton 
+      text={'버튼'} 
+      onClick={()=>alert("버튼 클릭")} 
+      // default 
+      // type={"negative"}
+    />
+
         {/* process.env.PUBLIC_URL로 적으면 public 폴더 안으로 들어간다 */}
         {/* <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} alt="" />
         <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} alt="" />
